b6b432845fb0ccd1ff01dfedae6a42062268c856
augustoams01 <augustoams01@gmail.com>
2026-05-16 01:54:08 -0300

    reafactor: product-card-adjust

--- frontend/src/components/ProductCard.tsx
+++ frontend/src/components/ProductCard.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
-
+import { api } from '../services/api'
 
 type ProductCardProps = {
+  id: number
   image: string
   title: string
   description: string
@@ -9,6 +10,7 @@ type ProductCardProps = {
 }
 
 export function ProductCard({
+  id,
   image,
   title,
   description,
@@ -16,6 +18,7 @@ export function ProductCard({
 }: ProductCardProps) {
 
   const [quantity, setQuantity] = useState(1)
+  const [loading, setLoading] = useState(false)
 
   function increaseQuantity() {
     setQuantity((prev) => prev + 1)
@@ -27,9 +30,51 @@ export function ProductCard({
     }
   }
 
+ async function addToCart() {
+
+  try {
+
+    setLoading(true)
+
+    let cartId: string | null = localStorage.getItem('cart_id')
+
+    // cria carrinho caso não exista
+
+    if (!cartId) {
+
+      const cartResponse = await api.post('/carts/')
+
+      cartId = String(cartResponse.data.id)
+
+      localStorage.setItem('cart_id', cartId)
+    }
+
+    // adiciona item
+
+    await api.post('/cart-items/', {
+      cart: cartId,
+      product_id: id,
+      quantity,
+    })
+
+    alert('Produto adicionado ao carrinho')
+
+  } catch (error) {
+
+    console.error(error)
+    alert('Erro ao adicionar produto')
+
+  } finally {
+
+    setLoading(false)
+
+  }
+}
+
   const totalPrice = price * quantity
 
   return (
+
     <div className="w-[380px] rounded-3xl p-6 flex flex-col items-center">
 
       <img
@@ -39,6 +84,7 @@ export function ProductCard({
       />
 
       <div className="mt-4 text-center">
+
         <h2 className="text-2xl font-semibold text-white">
           {title}
         </h2>
@@ -46,10 +92,11 @@ export function ProductCard({
         <p className="text-gray-400 text-sm mt-2 leading-relaxed">
           {description}
         </p>
+
       </div>
 
       <div className='flex gap-5'>
-  
+
         <div className="mt-6 flex items-center ">
 
           <button
@@ -59,27 +106,34 @@ export function ProductCard({
             -
           </button>
 
-          <span className="text-white border-t border-b border-b-gray-300/20 border-t-gray-300/20  bg-white/10 h-[80%] text-lg flex items-center z-50">
+          <span className="text-white border-t border-b border-b-gray-300/20 border-t-gray-300/20 bg-white/10 h-[80%] text-lg flex items-center z-50 px-1">
             {quantity}
           </span>
 
           <button
             onClick={increaseQuantity}
-            className="cursor-pointer  w-10 h-10 rounded-r-full border-r border-b border-t border-t-gray-300/20 border-b-gray-300/20 border-r-gray-300/20  bg-white/10 text-white text-xl"
+            className="cursor-pointer w-10 h-10 rounded-r-full border-r border-b border-t border-t-gray-300/20 border-b-gray-300/20 border-r-gray-300/20 bg-white/10 text-white text-xl"
           >
             +
           </button>
 
         </div>
 
-        <button className="mt-6 py-3 px-4 rounded-full bg-transparent border border-primary text-gray-300 hover:scale-[1.02] transition">
+        <button
+          onClick={addToCart}
+          disabled={loading}
+          className="mt-6 py-3 px-4 rounded-full bg-transparent border border-primary text-gray-300 hover:scale-[1.02] transition"
+        >
 
-          Add To Cart — R$ {totalPrice.toFixed(2)}
+          {
+            loading
+              ? 'Adding...'
+              : `Add To Cart — R$ ${totalPrice.toFixed(2)}`
+          }
 
         </button>
-      </div>
 
-     
+      </div>
 
     </div>
   )
