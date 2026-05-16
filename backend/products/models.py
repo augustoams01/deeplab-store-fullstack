2841ba71db88169865b4fb64dacbf6b6edd245de
augustoams01 <augustoams01@gmail.com>
2026-05-16 01:12:34 -0300

    feat: create-products-app-&-models

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    image = models.ImageField(
+        upload_to='products/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Cart(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        Cart,
+        related_name='items',
+        on_delete=models.CASCADE
+    )
+
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE
+    )
+
+    quantity = models.PositiveIntegerField(default=1)
