314922ca2f291ff51be026c4a389b6c747782d1c
augustoams01 <augustoams01@gmail.com>
2026-05-16 01:20:54 -0300

    feat: creat-serializers-&-admin-adjust

---
+++ backend/products/serializers.py
@@ -0,0 +1,39 @@
+from rest_framework import serializers
+from .models import Product, Cart, CartItem
+
+
+class ProductSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+
+class CartItemSerializer(serializers.ModelSerializer):
+
+    product = ProductSerializer(read_only=True)
+
+    product_id = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        source='product',
+        write_only=True
+    )
+
+    class Meta:
+        model = CartItem
+        fields = [
+            'id',
+            'cart',
+            'product',
+            'product_id',
+            'quantity'
+        ]
+
+
+class CartSerializer(serializers.ModelSerializer):
+
+    items = CartItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Cart
+        fields = '__all__'
